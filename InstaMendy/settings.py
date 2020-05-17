133a130c113d17f384c02d8599b2971d5bd25836
mengw3131 <mw3131@seas.upenn.edu>
2020-05-17 13:13:47 -0400

    deployment

--- InstaMendy/settings.py
+++ InstaMendy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3(lczgbmf8x#80cxl*x(6=wvv@+-r5_(=66m6lc+ikwuf5zd2@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'Insta',
     'imagekit',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
